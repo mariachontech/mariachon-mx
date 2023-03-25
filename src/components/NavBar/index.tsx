68f70104e77c9b0bb5f55d12872d0f3470216450
personal <jjtellezg@gmail.com>
2023-03-25 16:11:29 -0600

    nombre de imagen en mayuscula 3

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -6,13 +6,13 @@ type Props = {}
 const NavBar = (props: Props) => {
   return (
 		<div className="w-screen flex justify-end  items-center  mt-4 pr-5">
-				<div className="relative w-[83px] h-[36px] ">
-					<Image
-						className="object-cover"
-						src={"/images/menu.png"}
-						alt="Mariachon Company."
-						fill
-					/>
+			<div className="relative w-[83px] h-[36px] ">
+				<Image
+					className="object-cover"
+					src={"/images/pastelmenu.png"}
+					alt="Mariachon Company."
+					fill
+				/>
 			</div>
 		</div>
 	)
