f66cb0350047170eea83c0b85f0bf7384106c21b
personal <jjtellezg@gmail.com>
2023-06-18 13:59:44 -0600

    footer updated

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -20,15 +20,17 @@ const socials = [
 
 const Footer = (props: Props) => {
 	return (
-		<footer className="w-full py-4 bg-white">
-			<div className="container mx-auto flex justify-between items-center">
-				<div className="flex flex-col">
-					<h3 className="text-[#9A9A9A] text-2xl font-extrabold ">
+		<footer className="w-full h-fit my-4">
+			<div className="container mx-auto flex flex-col justify-end md:flex-row md:justify-between items-center">
+				<div className="hidden  md:flex md:flex-col">
+					<h3 className="text-[#9A9A9A] text-2xl font-extrabold 	 ">
 						Aviso de Privacidad{" "}
 					</h3>
-					<p className="text-[#9A9A9A]">Derechos reservados 2023 Mariachon</p>
+					<p className="text-[#9A9A9A] w-[40vw]">
+						Derechos reservados 2023 Mariachon
+					</p>
 				</div>
-				<div>
+				<div className="w-full text-end">
 					{socials.map((social) => (
 						<SocialIcon
 							key={social.id}
